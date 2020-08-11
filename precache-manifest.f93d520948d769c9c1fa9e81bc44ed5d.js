38e53fbcf297cee003a1b965dec4b2c0acb8e03f
Varun <kumarvarun9811@gmail.com>
2020-08-11 15:17:22 +0530

    Updates

---
+++ precache-manifest.f93d520948d769c9c1fa9e81bc44ed5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4724227e3063069c3e7139b7823b3699",
+    "url": "/kumarnurav9811/googlekeep/index.html"
+  },
+  {
+    "revision": "e3a0409f21b20cdc0457",
+    "url": "/kumarnurav9811/googlekeep/static/css/main.f31cc54a.chunk.css"
+  },
+  {
+    "revision": "390f9f0925137fe15581",
+    "url": "/kumarnurav9811/googlekeep/static/js/2.f2636578.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kumarnurav9811/googlekeep/static/js/2.f2636578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a0409f21b20cdc0457",
+    "url": "/kumarnurav9811/googlekeep/static/js/main.290b588b.chunk.js"
+  },
+  {
+    "revision": "a84e697a8c0e4d5a8a94",
+    "url": "/kumarnurav9811/googlekeep/static/js/runtime-main.b9e60d72.js"
+  },
+  {
+    "revision": "c6182a4a7e7d13ceb9b9153ff3ba7a88",
+    "url": "/kumarnurav9811/googlekeep/static/media/logo.c6182a4a.png"
+  }
+]);
